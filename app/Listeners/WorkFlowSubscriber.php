e4fae43ce424638296c1f7a7e9e27cf26a9ea098
k1fathi <javad.fathi@sarente.com>
2020-09-12 18:13:07 +0300

    workflow subscriber added

--- app/Listeners/WorkFlowSubscriber.php
+++ app/Listeners/WorkFlowSubscriber.php
@@ -5,8 +5,10 @@ namespace App\Listeners;
 use App\Models\Setting;
 
 use App\Models\User;
+use App\Models\UserWorkflow;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Support\Facades\Log;
 use Symfony\Component\Workflow\Event\GuardEvent;
 
 class WorkFlowSubscriber implements ShouldQueue
@@ -15,12 +17,15 @@ class WorkFlowSubscriber implements ShouldQueue
      * Handle workflow guard events.
      */
     public function onGuard(GuardEvent $event) {
+
+        $user_id=auth()->id();
+
         /** Symfony\Component\CustomWorkflow\Event\GuardEvent */
         $originalEvent = $event->getOriginalEvent();
 
-        /** @var App\Model\Activity $post */
-        $post = $originalEvent->getSubject();
-        $title = $post->title;
+        /** @var App\Models\UserWorkflow $user_workflow */
+        $user_workflow = $originalEvent->getSubject();
+        $title = $user_workflow->title;
 
         if (empty($title)) {
             // Posts with no title should not be allowed
@@ -31,22 +36,30 @@ class WorkFlowSubscriber implements ShouldQueue
     /**
      * Handle workflow leave event.
      */
-    public function onLeave($event) {}
+    public function onLeave($event) {
+        Log::info('onLeave');
+    }
 
     /**
      * Handle workflow transition event.
      */
-    public function onTransition($event) {}
+    public function onTransition($event) {
+        Log::info('onTransition');
+    }
 
     /**
      * Handle workflow enter event.
      */
-    public function onEnter($event) {}
+    public function onEnter($event) {
+        Log::info('onEnter');
+    }
 
     /**
      * Handle workflow entered event.
      */
-    public function onEntered($event) {}
+    public function onEntered($event) {
+        Log::info('onEntered');
+    }
 
     /**
      * Register the listeners for the subscriber.
@@ -56,27 +69,27 @@ class WorkFlowSubscriber implements ShouldQueue
     public function subscribe($events)
     {
         $events->listen(
-            'Vimily\LaravelWorkflow\Events\GuardEvent',
+            'ZeroDaHero\LaravelWorkflow\Events\GuardEvent',
             'App\Listeners\BlogPostWorkflowSubscriber@onGuard'
         );
 
         $events->listen(
-            'Vimily\LaravelWorkflow\Events\LeaveEvent',
+            'ZeroDaHero\LaravelWorkflow\Events\LeaveEvent',
             'App\Listeners\BlogPostWorkflowSubscriber@onLeave'
         );
 
         $events->listen(
-            'Vimily\LaravelWorkflow\Events\TransitionEvent',
+            'ZeroDaHero\LaravelWorkflow\Events\TransitionEvent',
             'App\Listeners\BlogPostWorkflowSubscriber@onTransition'
         );
 
         $events->listen(
-            'Vimily\LaravelWorkflow\Events\EnterEvent',
+            'ZeroDaHero\LaravelWorkflow\Events\EnterEvent',
             'App\Listeners\BlogPostWorkflowSubscriber@onEnter'
         );
 
         $events->listen(
-            'Vimily\LaravelWorkflow\Events\EnteredEvent',
+            'ZeroDaHero\LaravelWorkflow\Events\EnteredEvent',
             'App\Listeners\BlogPostWorkflowSubscriber@onEntered'
         );
     }
