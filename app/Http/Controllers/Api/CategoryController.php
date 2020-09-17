350b0f7f7c108393b705d551b95cbe8de2c1bd54
k1fathi <javad.fathi@sarente.com>
2020-09-17 17:28:10 +0300

    edit category index

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -16,40 +16,27 @@ class CategoryController extends Controller
         $lang = app()->getLocale();
         $result = collect();
         $user = User::getUser();
-
         $categories = Category::query();
-        $levels = Level::query();
-
         // ["category_id": 1,"current_point": "50"],...
         $user_category_points = $user->pointsByCategory()->toArray();
-
         //Get level by point of category
         foreach ($categories->pluck('id')->toArray() as $key => $value) {
-
             $category_name = Category::find($value)->translations['name'][$lang];
-
             if (array_key_exists($key, $user_category_points)
                 && $user_category_points[$key]['category_id'] == $value) {
                 //Get max point of each level { "id": 10,"level_no": 4,"level_point": 800,"category_id": 2}},....
                 $levels = Level::where('category_id', $value);
-
                 if ($levels->exists()) {
                     //Check $usr_cat_pnt['current_point'] in
                     $slected_level = $this->getLevelOfUserByPoint($user_category_points[$key]['current_point'], $levels->get()->toArray());
-
-
                     //sort data of array
                     $slected_level['current_point'] = (int)$user_category_points[$key]['current_point'];
                     //$slected_level['level_id']=$slected_level['id'];
                     $slected_level['category_name'] = $category_name;
                     unset($slected_level['id']);
-
                     $result->push($slected_level);
-
                 }
             } else {
-                //TODO:If user has any point in specific category
-                //$result->push([$user_category_points[$i], $slected_level]);
                 $null_poin_category = [
                     "level_no" => 0,
                     "level_point" => 0,
