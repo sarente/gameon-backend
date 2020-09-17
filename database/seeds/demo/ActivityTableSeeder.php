0ea047684e1db8da6a79b7cbd6fbc4264ac406c4
k1fathi <javad.fathi@sarente.com>
2020-09-17 11:24:08 +0300

    add activity type

--- database/seeds/demo/ActivityTableSeeder.php
+++ database/seeds/demo/ActivityTableSeeder.php
@@ -2,6 +2,7 @@
 
 namespace App\Database\Seeds\Demo;
 use App\Models\Activity;
+use App\Models\Setting;
 use Illuminate\Database\Seeder;
 
 class ActivityTableSeeder extends Seeder
@@ -18,12 +19,16 @@ class ActivityTableSeeder extends Seeder
         \App\Models\Activity::truncate();
         \Illuminate\Support\Facades\DB::statement('SET FOREIGN_KEY_CHECKS=1;');
 
-        $activities = ['Activity1', 'Activity2', 'Activity3'];
+        $activities = ['ACTIVITY_RETURN1','ACTIVITY_ACTION1', 'ACTIVITY_ACTION2' ];
 
         foreach ($activities as $key => $activity) {
-
+            $act_type=Setting::$activity_types[0];
+            if($key==0)  {
+                $act_type=Setting::$activity_types[1];
+            }
             $activity = new Activity([
                 'name' => $activity,
+                'type' => $act_type,
                 'point' =>  rand(10,100),
             ]);
             $activity->save();
