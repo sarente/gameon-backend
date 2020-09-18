4a253359b2f58755cbf3a44a0d1340b7f67ae7b4
k1fathi <javad.fathi@sarente.com>
2020-09-18 12:08:54 +0300

    workflow edited (edited order to paces)

--- config/workflow.php
+++ config/workflow.php
@@ -9,19 +9,22 @@ return [
         'places' => [
             'slide_show' => ['metadata' => [
                 'order' => '1',
-                'model_id' => '1',
+                'place_name' => 'Slideları İzle',
+                'model_id' => '2',
                 'model_type' => 'App\Models\Activity',
             ]
             ],
             'the_blanks' => ['metadata' => [
                 'order' => '2',
+                'place_name' => 'Değer İsmini Gir',
                 'model_id' => '2',
                 'model_type' => 'App\Models\Activity',
             ]
             ],
             'result' => ['metadata' => [
                 'order' => '3',
-                'model_id' => '3',
+                'place_name' => 'Sonuclari Gor',
+                'model_id' => '1',
                 'model_type' => 'App\Models\Reward',
             ]
             ],
@@ -54,19 +57,22 @@ return [
         'places' => [
             'slide_show' => ['metadata' => [
                 'order' => '1',
-                'model_id' => '1',
+                'place_name' => 'Slideları İzle',
+                'model_id' => '2',
                 'model_type' => 'App\Models\Activity',
             ]
             ],
             'the_blanks' => ['metadata' => [
                 'order' => '2',
+                'place_name' => 'Değer İsmini Gir',
                 'model_id' => '2',
                 'model_type' => 'App\Models\Activity',
             ]
             ],
             'result' => ['metadata' => [
                 'order' => '3',
-                'model_id' => '3',
+                'place_name' => 'Sonuclari Gor',
+                'model_id' => '1',
                 'model_type' => 'App\Models\Reward',
             ]
             ],
@@ -99,19 +105,22 @@ return [
         'places' => [
             'slide_show' => ['metadata' => [
                 'order' => '1',
-                'model_id' => '1',
+                'place_name' => 'Slideları İzle',
+                'model_id' => '2',
                 'model_type' => 'App\Models\Activity',
             ]
             ],
             'the_blanks' => ['metadata' => [
                 'order' => '2',
+                'place_name' => 'Değer İsmini Gir',
                 'model_id' => '2',
                 'model_type' => 'App\Models\Activity',
             ]
             ],
             'result' => ['metadata' => [
                 'order' => '3',
-                'model_id' => '3',
+                'place_name' => 'Sonuclari Gor',
+                'model_id' => '1',
                 'model_type' => 'App\Models\Reward',
             ]
             ],
@@ -144,19 +153,22 @@ return [
         'places' => [
             'slide_show' => ['metadata' => [
                 'order' => '1',
-                'model_id' => '1',
+                'place_name' => 'Slideları İzle',
+                'model_id' => '2',
                 'model_type' => 'App\Models\Activity',
             ]
             ],
             'the_blanks' => ['metadata' => [
                 'order' => '2',
+                'place_name' => 'Değer İsmini Gir',
                 'model_id' => '2',
                 'model_type' => 'App\Models\Activity',
             ]
             ],
             'result' => ['metadata' => [
                 'order' => '3',
-                'model_id' => '3',
+                'place_name' => 'Sonuclari Gor',
+                'model_id' => '1',
                 'model_type' => 'App\Models\Reward',
             ]
             ],
@@ -165,64 +177,67 @@ return [
             ]
             ]
         ],
-         'transitions' => [
-                'play_slide_show' => [
-                    'from' => 'slide_show',
-                    'to' => 'the_blanks',
-                ],
-                'fill_in_the_blanks' => [
-                    'from' => 'the_blanks',
-                    'to' => 'result'
-                ],
-                'show_result' => [
-                    'from' => 'result',
-                    'to' => 'done'
-                ]
+        'transitions' => [
+            'play_slide_show' => [
+                'from' => 'slide_show',
+                'to' => 'the_blanks',
+            ],
+            'fill_in_the_blanks' => [
+                'from' => 'the_blanks',
+                'to' => 'result'
+            ],
+            'show_result' => [
+                'from' => 'result',
+                'to' => 'done'
             ]
+        ]
     ],
     'entertainment' => [
-            'type' => 'workflow',
-            'metadata' => [
-                'title' => 'Eğlence',
+        'type' => 'workflow',
+        'metadata' => [
+            'title' => 'Eğlence',
+        ],
+        'supports' => ['App\Models\UserWorkflow'],
+        'places' => [
+            'slide_show' => ['metadata' => [
+                'order' => '1',
+                'place_name' => 'Slideları İzle',
+                'model_id' => '2',
+                'model_type' => 'App\Models\Activity',
+            ]
             ],
-            'supports' => ['App\Models\UserWorkflow'],
-            'places' => [
-                'slide_show' => ['metadata' => [
-                    'order' => '1',
-                    'model_id' => '1',
-                    'model_type' => 'App\Models\Activity',
-                ]
-                ],
-                'the_blanks' => ['metadata' => [
-                    'order' => '2',
-                    'model_id' => '2',
-                    'model_type' => 'App\Models\Activity',
-                ]
-                ],
-                'result' => ['metadata' => [
-                    'order' => '3',
-                    'model_id' => '3',
-                    'model_type' => 'App\Models\Reward',
-                ]
-                ],
-                'done' => ['metadata' => [
-                    'order' => '4',
-                ]
-                ]
+            'the_blanks' => ['metadata' => [
+                'order' => '2',
+                'place_name' => 'Değer İsmini Gir',
+                'model_id' => '2',
+                'model_type' => 'App\Models\Activity',
+            ]
             ],
-            'transitions' => [
-                    'play_slide_show' => [
-                        'from' => 'slide_show',
-                        'to' => 'the_blanks',
-                    ],
-                    'fill_in_the_blanks' => [
-                        'from' => 'the_blanks',
-                        'to' => 'result'
-                    ],
-                    'show_result' => [
-                        'from' => 'result',
-                        'to' => 'done'
-                    ]
+            'result' => ['metadata' => [
+                'order' => '3',
+                'place_name' => 'Sonuclari Gor',
+                'model_id' => '1',
+                'model_type' => 'App\Models\Reward',
+            ]
+            ],
+            'done' => ['metadata' => [
+                'order' => '4',
             ]
             ]
+        ],
+        'transitions' => [
+            'play_slide_show' => [
+                'from' => 'slide_show',
+                'to' => 'the_blanks',
+            ],
+            'fill_in_the_blanks' => [
+                'from' => 'the_blanks',
+                'to' => 'result'
+            ],
+            'show_result' => [
+                'from' => 'result',
+                'to' => 'done'
+            ]
+        ]
+    ]
 ];
