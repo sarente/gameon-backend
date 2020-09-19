109ef5f14d17c7be686124b0bf0633c7c4d8abc9
k1fathi <javad.fathi@sarente.com>
2020-09-19 21:57:40 +0300

    some optimization

--- app/Http/Controllers/Api/ActivityController.php
+++ app/Http/Controllers/Api/ActivityController.php
@@ -12,13 +12,15 @@ class ActivityController extends Controller
     {
         //Get workflow name
         //Load workflow
+        //Check activity name
         //Apply it
-        dd($workflow, $activity, request()->input());
-        $workflow = CustomWorkflow::where('id', $workflow_id);
+        //dd($workflow, $activity, request()->input());
 
+        $workflow = CustomWorkflow::where('id', $workflow);
         if (!$workflow->exists()) {
             throw new WorkFlowNotFoundException();
         }
+        // Activity
 
         return response()->success($workflow);
     }
