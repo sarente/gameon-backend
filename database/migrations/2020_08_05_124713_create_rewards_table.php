aa4a395e4c497195fea8977bbdc45d847fad423d
k1fathi <javad.fathi@sarente.com>
2020-09-19 21:54:24 +0300

    Reward table changed

--- database/migrations/2020_08_05_124713_create_rewards_table.php
+++ database/migrations/2020_08_05_124713_create_rewards_table.php
@@ -15,25 +15,9 @@ class CreateRewardsTable extends Migration
     {
         Schema::create('rewards', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('type')->nullable();
-            $table->timestamps();
-        });
-
-
-        Schema::create('reward_translations', function (Blueprint $table) {
-            $table->increments('id');
             $table->string('name');
             $table->text('description')->nullable();
-            $table->string('locale')->index();
-
-            $table->bigInteger('reward_id')->unsigned();
-            $table->foreign('reward_id')
-                ->references('id')
-                ->on('rewards')
-                ->onUpdate('cascade')
-                ->onDelete('cascade');
-
-            $table->unique(['reward_id', 'locale']);
+            $table->string('type')->nullable();
             $table->timestamps();
         });
     }
@@ -45,7 +29,6 @@ class CreateRewardsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('rewards_translations');
         Schema::dropIfExists('rewards');
     }
 }
