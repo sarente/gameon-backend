f02ef990f828736fbbeee44c2003025ed185c1bc
k1fathi <javad.fathi@sarente.com>
2020-09-27 13:54:50 +0300

    config file resolved

--- config/workflow0.php
+++ config/workflow0.php
@@ -1,37 +1,45 @@
 <?php
 return [
-    'straight' => [
-        'type' => 'workflow', // or 'state_machine'
+    'wf_01' => [
+        'type' => 'workflow',
         'metadata' => [
-            'title' => 'Blog Publishing Workflow',
-        ],
-        'marking_store' => [
-            'type' => 'multiple_state', // or 'single_state'
-            'property' => 'current_place' // this is the property on the model
+            'title' => 'GÜÇ BİRLİĞİ',
         ],
         'supports' => ['App\Models\UserWorkflow'],
         'places' => [
-            'draft',
-            'review',
-            'rejected',
-            'published'
-        ],
-        'transitions' => [
-            'to_review' => [
-                'from' => 'draft',
-                'to' => 'review',
-                'metadata' => [
-                    'priority' => 0.5,
+            'slide_show' => ['metadata' => [
+                'order' => '1',
+                'place_name' => 'Slaytları İzle',
+                'model_id' => '1',
+                'model_kind' => \App\Models\Setting::ACTIVITY_ACTION,
+                'model_type' => \App\Models\Activity::class,
+            ]
+            ],
+            'the_blanks' => ['metadata' => [
+                'order' => '2',
+                'place_name' => 'Kelimeyi Gir',
+                'model_id' => '2',
+                'model_kind' => \App\Models\Setting::ACTIVITY_RETURN,
+                'model_type' => \App\Models\Activity::class,
+            ],
+                'show_result' => ['metadata' => [
+                    'order' => '3',
+                    'place_name' => 'Neticeyi Gör',
+                    'model_id' => '1',
+                    'model_type' => \App\Models\ActivityResult::class,
                 ]
-            ],
-            'publish' => [
-                'from' => 'review',
-                'to' => 'published'
-            ],
-            'reject' => [
-                'from' => 'review',
-                'to' => 'rejected'
+                ]
             ]
         ],
+        'transitions' => [
+            'play_slide_show' => [
+                'from' => 'slide_show',
+                'to' => 'the_blanks',
+            ],
+            'fill_in_the_blanks' => [
+                'from' => 'the_blanks',
+                'to' => 'show_result',
+            ]
+        ]
     ]
 ];
