f82b9acc084baa61d77ac78d3fc7a943cf7daf5d
k1fathi <javad.fathi@sarente.com>
2020-09-28 11:09:56 +0300

    get activity types admin 1.0

--- app/Http/Controllers/Api/Admin/ActivityController.php
+++ app/Http/Controllers/Api/Admin/ActivityController.php
@@ -10,6 +10,7 @@ use App\Exceptions\WorkFlow\WorkFlowNotFoundException;
 use App\Http\Controllers\Controller;
 use App\Models\ActivityResult;
 use App\Models\CustomWorkflow;
+use App\Models\Setting;
 use App\Models\User;
 use App\Models\UserPoint;
 use App\Models\UserWorkflow;
@@ -19,6 +20,6 @@ use Symfony\Component\Workflow\Exception\LogicException;
 class ActivityController extends Controller
 {
     public function getActivityKinds(){
-
+        return response()->success(Setting::$activity_types);
     }
 }
