7d58b511a35cf3a2082b3943a63805908a62aace
k1fathi <javad.fathi@sarente.com>
2020-10-01 10:29:57 +0300

    ActivityResult changed to result 1.0

--- database/migrations/2019_08_29_072710_create_results_table.php
+++ database/migrations/2019_08_29_072710_create_results_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateActivityResultsTable extends Migration
+class CreateResultsTable extends Migration
 {
     /**
      * categories > workflow > activity > point
