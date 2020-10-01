dfb7b6708db1d05a1d547f0ca6a498abecc63be4
k1fathi <javad.fathi@sarente.com>
2020-10-01 14:31:29 +0300

    activity seeder changed

---
+++ app/Http/Controllers/Api/ActivityController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Exceptions\Activity\ActivityNotFoundException;
+use App\Http\Controllers\Controller;
+use App\Models\Activity;
+
+class ActivityController extends Controller
+{
+    public function show($activity)
+    {
+        $activity = Activity::with('image')->find($activity);
+        if (!$activity) {
+            throw new ActivityNotFoundException();
+        }
+        return response()->success($activity);
+    }
+}
